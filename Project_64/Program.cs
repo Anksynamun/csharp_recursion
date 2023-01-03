2a13ba03fa489dd22128a7de964016ab7a3100bd
Anna <Gorenskayaanna@mail.com>
2023-01-03 16:57:32 +0300

    Рекурсия

---
+++ Project_64/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите любое число: ");
+int n = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine(PrintNumbers(n, 1));
+string PrintNumbers(int end, int start)
+{
+    if (end == start) return end.ToString();
+    return (end + ", " + PrintNumbers(end - 1, start));
+}
---
+++ Project_66/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите начальное число: ");
+int n = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Введите конечное число: ");
+int m = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine(PrintNumber(n, m));
+int PrintNumber(int n, int m)
+{
+    if (n == m) return n;
+    return (n + PrintNumber(n + 1, m));
+}
